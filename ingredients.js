6336459c32bae7740b1542405baddd1eaf3e1b5d
Zeynep <zeynepkaya_26@hotmail.com>
2021-10-10 23:16:48 +0000

    ingredients looping

---
+++ ingredients.js
@@ -0,0 +1,18 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+};
+
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+};
+
+for (let i = 7; i > -1 ; i--) {
+  console.log(ingredients[i]);
+};
+
+
+
