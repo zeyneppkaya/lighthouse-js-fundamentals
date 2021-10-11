91fee8ebf1d15d30edc08c4e620fccff5b758204
Zeynep <zeynepkaya_26@hotmail.com>
2021-10-11 01:11:38 +0000

    concat

---
+++ concat.js
@@ -0,0 +1,8 @@
+const concat = function (firstArray, secondArray) {
+    let newarray = firstArray.concat(secondArray);
+    return newarray
+}
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
