a7b04e04e34052d392deb97ee219795de5f2355b
Zeynep <zeynepkaya_26@hotmail.com>
2021-10-11 01:12:05 +0000

    lastindexof

---
+++ lastIndexOf.js
@@ -0,0 +1,12 @@
+const lastIndexOf = function (start,end) {
+    for(let x = start.length; x >= 0; x--){
+      if(start[x]=== end){
+        return x;
+      } 
+    } return -1;
+};
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
